ad84d7122969ac3e1f417f9ccf9fc5970b0ac181
xjose999 <josejoestar80@gmail.com>
2026-04-14 21:39:43 -0500

    Ejercicio 3.1

---
+++ CajaFuerte.java
@@ -0,0 +1,10 @@
+public class CajaFuerte {
+    private double dinero;
+
+    public CajaFuerte(double dineroInicial) {
+        this.dinero = dineroInicial;
+    }
+
+    // NO hay getDinero()
+    // NO hay setDinero()
+}
